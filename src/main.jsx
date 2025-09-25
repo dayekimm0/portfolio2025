87842f4f4f6fe601a09a24026a4721b5da81328e
dayekimm0 <dayekimm0@gmail.com>
2025-09-25 20:26:25 +0900

    error

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,10 @@
 import { createRoot } from "react-dom/client";
 import Root from "./Root.jsx";
+import GlobalStyles from "./styles/GlobalStyles.styles.jsx";
 
-createRoot(document.getElementById("root")).render(<Root />);
+createRoot(document.getElementById("root")).render(
+  <>
+    <GlobalStyles />
+    <Root />
+  </>
+);
